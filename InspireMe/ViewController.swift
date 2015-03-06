be05386f09000cea694e03f7cd5c1d392730cf40
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-06 11:20:27 -0800

    Initial Commit

---
+++ InspireMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InspireMe
+//
+//  Created by Vineet Tiwari on 2015-03-06.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
